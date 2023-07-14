04ab27ae6deb54a4f0759bbcdb975a3cbc9fadaa
HenriqueCG81 <123783235+HenriqueCG81@users.noreply.github.com>
2023-07-14 14:48:53 +0100

    lets go

---
+++ controllers/doctorCtrl.js
@@ -0,0 +1,43 @@
+const doctorModel = require('../models/doctor.Models');
+
+const getDoctorInfoController = async (req, res) => {
+  try {
+    const doctor = await doctorModel.findOne({ userId: req.body.userId });
+    res.status(200).send({
+      success: true,
+      message: `Doctor info fetched successfully`,
+      data: doctor
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: `Error from get doctor info controller`,
+      error
+    });
+  }
+};
+//update profile
+
+const updateProfileController = async (req, res) => {
+  try {
+    const doctor = await doctorModel.findOneAndUpdate(
+      { userId: req.body.userId },
+      req.body
+    );
+    res.status(201).send({
+      success: true,
+      message: `Doctor profile updated successfully`,
+      data: doctor
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: `Error from update profile controller`,
+      error
+    });
+  }
+};
+
+module.exports = { getDoctorInfoController, updateProfileController };
---
+++ routes/doctorRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/authMiddleware');
+const {
+  getDoctorInfoController,
+  updateProfileController
+} = require('../controllers/doctorCtrl');
+
+const router = express.Router();
+//post single doctor
+
+router.post('/getDoctorInfo', authMiddleware, getDoctorInfoController);
+
+//post update profile
+router.post('/updateProfile', authMiddleware, updateProfileController);
+
+module.exports = router;
